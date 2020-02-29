3d92ba996c8853d71b03faa782eb2d7cf6712a76
autumnFish <517729329@qq.com>
2020-02-29 21:50:01 +0800

    合并css 调整 songs页面的table

--- src/views/result.vue
+++ src/views/result.vue
@@ -108,7 +108,7 @@
         </div>
       </el-tab-pane>
       <el-tab-pane label="MV" name="mv">
-        <div class="items">
+        <div class="items mv">
           <div class="item">
             <div class="img-wrap">
               <img src="../assets/mvCover.jpg" alt="" />
@@ -203,98 +203,5 @@ export default {
 </script>
 
 <style lang="scss">
-.result-container {
-  .title-wrap {
-    display: flex;
-    align-items: flex-end;
-    margin-bottom: 30px;
-    .title {
-      margin-right: 10px;
-    }
-    .sub-title {
-      font-size: 15px;
-      color: #bebebe;
-    }
-  }
-  .el-tabs__item {
-    font-size: 18px;
-  }
-  .el-table td,
-  .el-table th.is-leaf {
-    border-bottom: none;
-  }
-  .items {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    .item {
-      width: 200px;
-      cursor: pointer;
-      margin-right: 20px;
-      margin-bottom: 20px;
-      .img-wrap {
-        width: 100%;
-        position: relative;
-        > .icon-play {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          width: 40px;
-          height: 40px;
-          color: #dd6d60;
-          font-size: 20px;
-          border-radius: 50%;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background: rgba(255, 255, 255, 0.8);
-          opacity: 0;
-          &::before {
-            transform: translateX(3px);
-          }
-        }
-        &:hover > .icon-play {
-          opacity: 1;
-        }
-        img {
-          width: 100%;
-          border-radius: 5px;
-        }
-        .num-wrap {
-          position: absolute;
-          color: white;
-          top: 0;
-          right: 0;
-          display: flex;
-          align-items: center;
-          font-size: 15px;
-          padding-right: 5px;
-          padding-top: 2px;
-          .icon-play {
-            font-size: 12px;
-            display: flex;
-            align-items: center;
-            margin-right: 5px;
-          }
-        }
-        .time {
-          position: absolute;
-          bottom: 5px;
-          right: 5px;
-          color: white;
-          font-size: 15px;
-        }
-      }
-      .name {
-        font-size: 15px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;
-      }
-    }
-  }
-}
+
 </style>
--- src/views/songs.vue
+++ src/views/songs.vue
@@ -8,31 +8,59 @@
       <span class="item">韩国</span>
     </div>
     <!-- 底部的table -->
-    <el-table class="song-table" :data="tableData">
-      <el-table-column type="index"></el-table-column>
-      <el-table-column width="100">
-        <template slot-scope="scope">
-          <div class="img-wrap">
-            <img :src="scope.row.img" alt="" />
-            <span class="iconfont icon-play"></span>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column width="280">
-        <template slot-scope="scope">
-          <div class="song-wrap">
-            <div class="name-wrap">
-              <span>{{ scope.row.songName }}</span>
-              <span v-if="scope.row.hasMV != 0" class="iconfont icon-mv"></span>
+    <table class="el-table playlit-table">
+      <thead>
+        <th></th>
+        <th></th>
+        <th>音乐标题</th>
+        <th>歌手</th>
+        <th>专辑</th>
+        <th>时长</th>
+      </thead>
+      <tbody>
+        <tr class="el-table__row">
+          <td>1</td>
+          <td>
+            <div class="img-wrap">
+              <img src="../assets/songCover.jpg" alt="" />
+              <span class="iconfont icon-play"></span>
             </div>
-            <span>{{ scope.row.subTitle }}</span>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column width="280" prop="singer"></el-table-column>
-      <el-table-column width="280" prop="albumName"></el-table-column>
-      <el-table-column prop="duration"></el-table-column>
-    </el-table>
+          </td>
+          <td>
+            <div class="song-wrap">
+              <div class="name-wrap">
+                <span>你要相信这不是最后一天</span>
+                <span class="iconfont icon-mv"></span>
+              </div>
+              <span>电视剧加油练习生插曲</span>
+            </div>
+          </td>
+          <td>华晨宇</td>
+          <td>你要相信这不是最后一天</td>
+          <td>06:03</td>
+        </tr>
+        <tr class="el-table__row">
+          <td>2</td>
+          <td>
+            <div class="img-wrap">
+              <img src="../assets/songCover.jpg" alt="" />
+              <span class="iconfont icon-play"></span>
+            </div>
+          </td>
+          <td>
+            <div class="song-wrap">
+              <div class="name-wrap">
+                <span>你要相信这不是最后一天</span>
+                <span class="iconfont icon-mv"></span>
+              </div>
+            </div>
+          </td>
+          <td>华晨宇</td>
+          <td>你要相信这不是最后一天</td>
+          <td>06:03</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -41,96 +69,12 @@ export default {
   name: 'songs',
   data() {
     return {
-      tableData: [
-        {
-          img:
-            'http://p3.music.126.net/sL4dfIANkKupkJvPipmd5g==/109951164736488659.jpg?param=120y120',
-          songName: '你要相信这不是最后一天',
-          subTitle: '电视剧《加油吧实习生》插曲',
-          singer: '华晨宇',
-          albumName: '你要相信这不是最后一天',
-          duration: '06:03',
-          hasMV: 0
-        },
-        {
-          img:
-            'http://p4.music.126.net/76Hpk_9ot2h2dozv5JbbYA==/109951164737016168.jpg?param=120y120',
-          songName: 'Tomorrow will be fine.',
-          singer: 'Sodagreen',
-          albumName: 'Tomorrow will be fine.',
-          duration: '04:59',
-          hasMV: 1
-        },
-        {
-          img:
-            'http://p4.music.126.net/QI_lE_SlqUXa51z56qcOSw==/109951164732550113.jpg?param=120y120',
-          songName: '误解',
-          singer: '戴佩妮',
-          albumName: '误解',
-          duration: '03:43',
-          hasMV: 1
-        }
-      ]
+ 
     };
   }
 };
 </script>
 
 <style lang="scss">
-.songs-container {
-  .tab-bar {
-    display: flex;
-    justify-content: flex-end;
-    .item {
-      font-size: 15px;
-      color: gray;
-      margin-right: 20px;
-      cursor: pointer;
-      &.active {
-        color: #dd6d60;
-      }
-    }
-  }
-  .song-table {
-    .song-wrap {
-      > span {
-        margin-top: 20px;
-        display: inline-block;
-        color: #bebebe;
-      }
-      .icon-mv {
-        padding-left: 5px;
-        color: #dd6d60;
-      }
-    }
-    .img-wrap {
-      position: relative;
-      width: 70px;
-      height: 70px;
-      img {
-        width: 70px;
-        height: 70px;
-        border-radius: 5px;
-      }
-      .icon-play {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 25px;
-        height: 25px;
-        color: #dd6d60;
-        font-size: 12px;
-        border-radius: 50%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: rgba(255, 255, 255, 0.8);
-        &::before {
-          transform: translateX(1px);
-        }
-      }
-    }
-  }
-}
+
 </style>
